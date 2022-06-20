aff44572ad01a306fe7e5ab0e875d2aa52f39060
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-06-20 12:53:51 +0400

    First Commit

---
+++ P129Allup/P129Allup/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P129Allup.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P129Allup.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class ProductController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(string name1, string surname, int age, string email)
+        {
+            return Content($"{name1} {surname} {age} {email}");
+        }
+    }
+}
---
+++ P129Allup/P129Allup/Models/Brand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace P129Allup.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength:255)]
+        public string Name { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
